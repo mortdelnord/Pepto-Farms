0937ace85bdc2bd649e5cded315c0374b946f04f
Elijah Rivas <elijah.a.rivas@gmail.com>
2024-04-23 19:44:23 -0400

    fixed a navmesh issue

--- Assets/NavMeshLinkResizer.cs
+++ Assets/NavMeshLinkResizer.cs
@@ -18,20 +18,20 @@ public class NavMeshLinkResizer : MonoBehaviour
         m_Links = gameObject.GetComponents<NavMeshLink>();
         foreach(NavMeshLink link in m_Links)
         {
-            link.width = thisCollider.bounds.size.x;
+            link.width = thisCollider.bounds.size.x - 0.5f;
         }
         NavMeshLink xLink = m_Links[0];
         NavMeshLink zLink = m_Links[1];
 
-        xLink.startPoint = new Vector3(thisCollider.GetComponent<BoxCollider>().center.x, 0f, -3f);
-        xLink.endPoint = new Vector3(thisCollider.GetComponent<BoxCollider>().center.x, 0f, 3f);
+        xLink.startPoint = new Vector3(thisCollider.GetComponent<BoxCollider>().center.x, 0f, -2f);
+        xLink.endPoint = new Vector3(thisCollider.GetComponent<BoxCollider>().center.x, 0f, 2f);
 
-        zLink.startPoint = new Vector3(3f, 0f, thisCollider.GetComponent<BoxCollider>().center.z);
-        zLink.endPoint = new Vector3(-3f, 0f, thisCollider.GetComponent<BoxCollider>().center.z);
+        zLink.startPoint = new Vector3(2f, 0f, thisCollider.GetComponent<BoxCollider>().center.z);
+        zLink.endPoint = new Vector3(-2f, 0f, thisCollider.GetComponent<BoxCollider>().center.z);
 
-        xLink.width = thisCollider.bounds.size.x;
+        xLink.width = thisCollider.bounds.size.x - 0.5f;
         //Debug.Log(xLink.width);
-        zLink.width = thisCollider.bounds.size.z;
+        zLink.width = thisCollider.bounds.size.z -0.5f;
 
         float minWidth = Mathf.Min(zLink.width, xLink.width);
         if (minWidth == xLink.width)
